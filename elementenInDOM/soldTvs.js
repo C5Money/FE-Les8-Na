2ee9e2d719d0f01aed4b4f7dd84d12553a3be682
C5Money <misriffi@gmail.com>
2022-11-07 23:48:20 +0100

    Tot zover

--- elementenInDOM/soldTvs.js
+++
@@ -1,6 +0,0 @@
-const verkochteTvs = inventory.filter((inventoryList) => {
-    for(let i = 0; inventoryList.sold; i++){
-
-    }
-});
-console.log();
---
+++ elementsInDom.js
@@ -0,0 +1,9 @@
+const verkochteTvs = inventory.map((inventoryList) => {
+    let sum = 0;
+    for (let i = 0; i < inventoryList.sold; i++){
+        sum += inventoryList.sold;
+
+    }
+
+});
+console.log(verkochteTvs);
