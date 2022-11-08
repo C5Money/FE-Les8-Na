5376e4c7347e255cc555ae5d28169ad886ccbcbd
C5Money <misriffi@gmail.com>
2022-11-09 00:20:58 +0100

    smartphone

--- elementsInDom.js
+++ elementsInDom.js
@@ -1,9 +1,32 @@
-const verkochteTvs = inventory.map((inventoryList) => {
-    let sum = 0;
-    for (let i = 0; i < inventoryList.sold; i++){
-        sum += inventoryList.sold;
+// Totall Tv's sold
+let sumSold = 0;
+for (let i = 0; i < inventory.length; i++){
+    sumSold += inventory[i].sold;
+}
+console.log(sumSold);
 
-    }
 
-});
-console.log(verkochteTvs);
+
+// Dynamisch injecteren tv's sold GROEN
+const tvsSold = document.createElement("h1");
+tvsSold.tekstContent = "Titel Boek";
+// const hallo = "";
+// hallo.appendChild(tvsSold);
+
+
+
+// Totall Tv's bought
+let sumBougth = 0;
+for (let i = 0; i < inventory.length; i++){
+    sumBougth += inventory[1].originalStock;
+}
+console.log(sumBougth);
+
+
+
+// Dynamisch injecteren tv's bought BLAUW
+
+
+
+
+//
